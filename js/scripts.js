e567c6962f4bd8f0d095ee0b587b91867422885f
Your Name <brannerchinese@users.noreply.github.com>
2017-05-05 21:23:39 -0400

    Add random number functionality, without callbacks yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   // Function calls
 
   // Globals
@@ -30,6 +30,29 @@
     return stack;
   }
 
+  // Generate sufficiently large random range
+  function randomRange(min=100000001, max=100000000000000000000001) {
+    return Math.floor(Math.random() * (max-min)) + min;
+  }
+
+  function fn(value, factors) {
+    counter = 1;
+    while (value == factors.reduce(function(acc, val) { return acc * val; }, 1)) {
+      var value = randomRange();
+      var factors = factorStack(value);
+      counter += 1;
+      console.log('#', counter, ': ', value);
+    };
+    console.log('Error factoring ', value, ' into ', factors);
+  };
+  //
+  // Factor and test number
+  function factorAndTest() {
+    var value = 2;
+    var factors = factorStack(value);
+    fn(value, factors);
+  }
+
   // Post entity to window
   function post(entity) {
   }
@@ -37,4 +60,4 @@
   // Animate sending and return of asynchronous function
   function animate() {
   }
-});
+};
